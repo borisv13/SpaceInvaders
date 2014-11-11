87e209989a0f64d3fc31685cf5f67b9c5cde79ba
borisv13 <borisv13@gmail.com>
2014-11-11 09:39:10 -0500

    Refactoring LevelCreator.

--- SpaceInvaders/src/LevelCreator.java
+++ SpaceInvaders/src/LevelCreator.java
@@ -4,13 +4,20 @@ import java.util.List;
 
 
 public class LevelCreator {
+	
+	private static final int ScreenHeightDivisor = 5;
+	private static final int AlienHeightSeperator = 2;
+	private static final int AlienWidthSeperator = 3;
+	private static final int AlienWidthBufferFactor = 2;
 
 	public static List<Alien> getAliens(int screenWidth, int screenHeight) {
 		List<Alien> aliens = new ArrayList<Alien>();
 		BufferedImage alienImage = Factory.getImageHandler().getAlien();
-		for (int yCoord = alienImage.getHeight(null); yCoord < (screenHeight/5)+alienImage.getHeight(null); yCoord += alienImage.getHeight(null)*2)
+		int height = alienImage.getHeight();
+		int width = alienImage.getWidth();
+		for (int yCoord = height; yCoord < (screenHeight/ScreenHeightDivisor)+height; yCoord += height*AlienHeightSeperator)
 		{
-			for (int xCoord = alienImage.getWidth(null)*2; xCoord+alienImage.getWidth(null) < screenWidth; xCoord += alienImage.getWidth(null)*3)
+			for (int xCoord = width*AlienWidthBufferFactor; xCoord+width < screenWidth; xCoord += width*AlienWidthSeperator)
 			{
 				aliens.add(Factory.createAlien(xCoord, yCoord, screenWidth));
 			}
