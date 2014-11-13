5e967b7a128b9fc295f8c60803a04bc56afbba81
cmwestbr <westbrook.chris@gmail.com>
2014-11-13 13:16:30 -0500

    Refactored TunableParameters to have 1 place for setting speeds based on reference speed. Reduced the ship speed.

--- SpaceInvaders/src/TunableParameters.java
+++ SpaceInvaders/src/TunableParameters.java
@@ -1,10 +1,10 @@
 
 public class TunableParameters {
-	public static int ReferenceSpeed = 1;
-	public static int ShipSpeed = TunableParameters.ReferenceSpeed * 10;
-	public static int BackgroundSpeed = TunableParameters.ReferenceSpeed;
-	public static int AlienSpeed = TunableParameters.ReferenceSpeed * 2;
-	public static int AlienVerticalSpeed = TunableParameters.ReferenceSpeed * 10;
+	public static int ReferenceSpeed;
+	public static int ShipSpeed;
+	public static int BackgroundSpeed;
+	public static int AlienSpeed;
+	public static int AlienVerticalSpeed;
 	public static int MissileSpeed = 3;
 	public static int AlienScore = 10;
 	public static int PercentChanceAlienFiresMissile = 1;
@@ -12,10 +12,16 @@ public class TunableParameters {
 	public static int ScreenWidth = 800;
 	public static int ScreenHeight = 600;
 	
+	static {
+		SetReferenceSpeed(1);
+	}
+	
 	public static void SetReferenceSpeed(int newSpeed) {
 			TunableParameters.ReferenceSpeed = newSpeed;
-			TunableParameters.ShipSpeed = newSpeed * 10;
+			TunableParameters.ShipSpeed = newSpeed * 2;
 			TunableParameters.BackgroundSpeed = newSpeed;
 			TunableParameters.AlienSpeed = newSpeed * 2;
+			TunableParameters.AlienVerticalSpeed = newSpeed * 10;
 	}
+		
 }
