ff0197befa8bd59246d05ab029582a0dd3e26ddc
cmwestbr <westbrook.chris@gmail.com>
2014-11-18 19:53:08 -0500

    Added test package with first test case class

---
+++ SpaceInvaders/src/test/AlienTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlienTest {
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFireMissile() {
+		fail("Not yet implemented");
+	}
+
+}
