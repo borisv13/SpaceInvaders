10afd9ea62124b1a8fafe84e62595eabf2d09bd2
cmwestbr <westbrook.chris@gmail.com>
2014-12-01 17:04:15 -0500

    Previous frame rate code was a busy loop and didn't give CPU any rest. To reduce CPU and power usage added a sleep call based on time till next run with a buffer to account for sleep non-determinism.

--- SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
+++ SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
@@ -20,6 +20,7 @@ public class SpaceInvaders implements Runnable {
 		long last = 0;
 		int fps = TunableParameters.TargetFPS;
 		long framePeriodNS = 1000000000/fps; 
+		long timeToSleepMS;
 		
 		while(true) {
 			now = System.nanoTime();
@@ -29,8 +30,19 @@ public class SpaceInvaders implements Runnable {
 				if(this.gameFrame.run()) {
 					gameFrame.dispose();
 					gameFrame = new GameFrame();
-				}				
-			}						
+				}
+				// Next we sleep a portion of the time until the next schedule frame run to give the CPU a break
+				// However because of non-determinism in the sleep function we don't get closer than 8 ms to next run time.
+				timeToSleepMS = (long) Math.floor((last + framePeriodNS - System.nanoTime()) / 1000000) - 8;
+				if (timeToSleepMS > 0) {
+					try {
+						Thread.sleep(timeToSleepMS);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
 		}
 	}
 }
